9efc8e2483f190c2ef257c3fe84168ecd6a62f51
marcio cardoso <137656788+Viniciosrdz@users.noreply.github.com>
2026-01-04 20:02:31 +0000

    separacao de dados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,21 +6,12 @@
 
 int main() {
 // Área para definição das variáveis para armazenar as propriedades das cidades
-
-  int codigo1 = 1;
-  int estado1 = 'A';
-  char carta1[] = "A01";
   char cidade1[] = "São Paulo";
   int populacao = 12325000;
   float area = 1521.11;
   float pib = populacao / area;
   int pontos_turisticos = 50;
-
-  printf("\n");
-
-  int codigo2 = 2;
-  int estado2 = 'B';
-  char carta2[] = "B02";
+ 
   char cidade2[] = "Rio de Janeiro";
   int populacao2 = 6748000;
   float area2 = 1200.25;
@@ -28,6 +19,18 @@ int main() {
   int pontos_turisticos2 = 30;
 
 // Área para entrada de dados
+
+  int codigo1 = 1;
+  int estado1 = 'A';
+  char carta1[] = "A01";
+
+  int codigo2 = 2;
+  int estado2 = 'B';
+  char carta2[] = "B02";
+  
+
+// Área para exibição dos dados da cidade
+
   printf("Carta %d\n", codigo1);
   printf("Estado %c\n", estado1);
   printf("Codigo %s\n", carta1);
@@ -48,7 +51,6 @@ int main() {
   printf("PIB %.2f\n", pib2);
   printf("Pontos Turisticos %d\n", pontos_turisticos2);
 
-// Área para exibição dos dados da cidade
 
 return 0;
 } 
