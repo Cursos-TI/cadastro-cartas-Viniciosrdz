1de4d0a39840ad85068daee1c1649e069d83a7d8
marcio cardoso <137656788+Viniciosrdz@users.noreply.github.com>
2026-01-13 18:46:33 +0000

    feito inclusao do desafio intermediario

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,14 +8,18 @@ int main() {
   char cidade1[] = "São Paulo";
   int populacao = 12325000;
   float area = 1521.11;
-  float pib = populacao / area;
-  int pontos_turisticos = 50;
+  float pib = 699.28;
+  int pontosturisticos = 50;
+  float densidadepopulacional = populacao / area;  
+  float pibpercapita = pib / populacao;
  
   char cidade2[] = "Rio de Janeiro";
   int populacao2 = 6748000;
-  float area2 = 1200.25;
-  float pib2 = populacao2 / area2;
-  int pontos_turisticos2 = 30;
+  float area2 = 1200.25;  
+  float pib2 = 300.50;
+  int pontosturisticos2 = 30;  
+  float densidadepopulacional2 = populacao2 / area2;  
+  float pibpercapita2 = pib2 / populacao2;
 
 // Área para entrada de dados
 
@@ -36,7 +40,9 @@ int main() {
   printf("Populacao - %d\n", populacao);
   printf("Area - %.2f\n", area);
   printf("PIB - %.2f\n", pib);
-  printf("Pontos Turisticos - %d\n", pontos_turisticos);
+  printf("Pontos Turisticos - %d\n", pontosturisticos);
+  printf("Densidade Populacional - %f\n", densidadepopulacional);
+  printf("PIB per Capita - %f\n", pibpercapita);
 
   printf("\n");
 
@@ -47,7 +53,9 @@ int main() {
   printf("Populacao - %d\n", populacao2);
   printf("Area - %.2f\n", area2);
   printf("PIB - %.2f\n", pib2);
-  printf("Pontos Turisticos - %d\n", pontos_turisticos2);
+  printf("Pontos Turisticos - %d\n", pontosturisticos2);
+  printf("Densidade Populacional - %f\n", densidadepopulacional2);
+  printf("PIB per Capita - %f\n", pibpercapita2);
 
 return 0;
 } 
